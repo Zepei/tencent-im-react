bd340f48ad3bcc2e9cee364b483ab6b0b998b0fe
Zepei Xu <xuzepei19950617@gmail.com>
2019-03-27 13:25:13 -0400

    imported TIM

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'tencent-im-react'
+import './App.css'
+import Tim from 'tencent-im-react'
 
 export default class App extends Component {
   render () {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div className="App">
+        <Tim/>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './styles.css'
+import classes from './index.module.css';
 
-export default class ExampleComponent extends Component {
+export default class Tim extends Component {
   static propTypes = {
     text: PropTypes.string
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div className={classes.Tim}>
+        <div className={classes.ChatLeft}>
+          <div className={classes.TopBar}></div>
+          <div className={classes.Sessions}></div>
+        </div>
+        <div className={classes.ChatRight}>
+          <div className={classes.Title}></div>
+          <div className={classes.Content}></div> 
+          <div className={classes.TextEditor}></div>
+        </div>
       </div>
     )
   }
